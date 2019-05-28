292be35053763ee593b5721bf7ce450ec4dc1603
abbashan03 <42873886+abbashan03@users.noreply.github.com>
2019-05-28 18:53:32 +0200

    reloaud

--- grafisches interface.py
+++ grafisches interface.py
@@ -1,11 +1,13 @@
 # import um fenster zu erstellen
 from tkinter import *
-from tkinter import messagebox
+
 
 # folgende Funktion soll ausgeführt werden, wenn
 # der Benutzer den Button anklickt
 def Start_action():
-    Ergebnis_label.config(text="Fertig")
+    f = open("test","w+")
+    f.write()
+    f.close
     
 def controlle_action():
     coreckt_label.config(text="coreckt")
@@ -22,7 +24,8 @@ coreckt_label = Label(fenster, text="")
 
 
 # Eingabe
-eingabefeld = Entry(fenster, bd=5, width=70)
+t = Text(fenster, bd=5, height=20, width=54)
+t.pack()
 
 
 # definieren der Grösse des Fensters
@@ -33,12 +36,16 @@ Ergebnis_label.place(x = 350, y = 300, width=50, height=50)
 Start_button.place(x = 10, y = 0, width=50, height=50)
 coreckt_label.place(x = 350, y = 260, width=50, height=50)
 controlle_button.place(x = 60, y = 0, width=80, height=50)
-eingabefeld.place(x = 3, y = 60)
+t.place(x = 3, y = 60)
 
 # wartet auf eingabe
 fenster.mainloop()
 
 #https://pythonbuch.com/gui.html
+#https://www.devdungeon.com/content/gui-programming-python
+#https://stackoverflow.com/questions/24501606/tkinter-python-entry-height
+#https://www.guru99.com/reading-and-writing-files-in-python.html#1
+#https://www.pythoncentral.io/reading-and-writing-to-files-in-python/
 
 
 
@@ -73,5 +80,4 @@ fenster.mainloop()
 
 
 
-#Quelle
-#https://pythonbuch.com/gui.html
+
